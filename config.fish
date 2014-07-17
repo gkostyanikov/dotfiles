26229fc6ce92e65a98815ab56af23570e14ae60a
gkostyanikov <heypandora@gmail.com>
2014-07-17 23:48:30 +0400

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 set fish_greeting
 set -x PATH /usr/local/bin $PATH
 set -x PATH $HOME/.rbenv/bin $PATH
-set -x PATH $HOME/.rbenv/shims 
+set -x PATH $HOME/.rbenv/shims $PATH
 set -x PATH $HOME/repo/go/bin $PATH
 set -x GOPATH $HOME/repo/go
 rbenv rehash >/dev/null ^&1
